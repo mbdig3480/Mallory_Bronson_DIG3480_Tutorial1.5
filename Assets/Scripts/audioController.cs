0f2e5d76aab18744e2966f55c8bea7dabcf18a27
Mallory Bronson <mallorybronson@Mallorys-MacBook-Pro.local>
2021-02-18 14:16:13 -0500

    Animation commit

---
+++ Assets/Scripts/audioController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    
+   // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }   
+
+        if (Input.GetKeyDown(KeyCode.W))
+            {
+                musicSource.clip = musicClipOne;
+                musicSource.Play();
+
+            }
+
+        if (Input.GetKeyUp(KeyCode.W))
+            {
+                musicSource.Stop();
+
+            }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            {
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+            }
+
+        if (Input.GetKeyUp(KeyCode.R))
+            {
+                musicSource.Stop();
+
+            }
+
+        if (Input.GetKeyDown(KeyCode.L))
+            {
+                musicSource.loop = true;
+            }
+
+        if (Input.GetKeyUp(KeyCode.L))
+            {
+                musicSource.loop = false;
+            }
+   }
+}
